1b984988d502650efe9bb51895f9e053023953c8
Amit Kumar <sahaiamit172@gmail.com>
2025-03-07 21:32:09 +0530

    splash activity and login added

---
+++ app/src/main/java/com/example/nexus/activity/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.example.nexus.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.nexus.R
+import com.example.nexus.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+
+        }
+
+        binding.btnSignup.setOnClickListener {
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/nexus/activity/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.nexus.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.nexus.R
+
+class SplashActivity : AppCompatActivity() {
+    private val delay = 1300L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        },delay)
+
+    }
+}
