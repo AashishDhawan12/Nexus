3ea91b9a60600e1006bace0ebb4fe84b8ec66ac6
Amit Kumar <sahaiamit172@gmail.com>
2025-03-08 14:35:47 +0530

    bug

--- app/src/main/java/com/example/nexus/activity/LoginActivity.kt
+++ app/src/main/java/com/example/nexus/activity/LoginActivity.kt
@@ -5,16 +5,30 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nexus.R
 import com.example.nexus.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
         binding.btnLogin.setOnClickListener {
-            Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show()
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) {
+                task ->
+                    if(task.isSuccessful){
+                        val user = auth.currentUser
+                        Toast.makeText(this,"Looged in suxxfully",Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this,"Something went wrong",Toast.LENGTH_SHORT).show()
+                    }
+            }
         }
 
         binding.btnSignup.setOnClickListener {
