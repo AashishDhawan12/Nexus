ee2fd4605c70869bdfc1424d0b0929994252e4c8
Amit Kumar <sahaiamit172@gmail.com>
2025-03-09 22:36:29 +0530

    home activity image fix

--- app/src/main/java/com/example/nexus/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/nexus/activity/SignUpActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nexus.databinding.ActivitySignUpBinding
--- app/src/main/java/com/example/nexus/adapter/HomeAdapter.kt
+++ app/src/main/java/com/example/nexus/adapter/HomeAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.nexus.adapter
 
+import android.graphics.BitmapFactory
+import android.util.Base64
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.collection.intIntMapOf
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nexus.databinding.SingleChatItemBinding
 import com.example.nexus.model.UserData
 
-class HomeAdapter (private val list: MutableList<UserData>): RecyclerView.Adapter<HomeAdapter.ViewModel>() {
+class HomeAdapter (private val data: MutableList<UserData>): RecyclerView.Adapter<HomeAdapter.ViewModel>() {
     inner class ViewModel (val binding: SingleChatItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewModel {
@@ -15,12 +18,16 @@ class HomeAdapter (private val list: MutableList<UserData>): RecyclerView.Adapte
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return data.size
     }
 
     override fun onBindViewHolder(holder: ViewModel, position: Int) {
-//        holder.binding.imgUser.setImageBitmap(list[position].image)
-        holder.binding.txtUserName.text = list[position].userName
+        val curr = data[position]
+        val decodedBytes = Base64.decode(curr.image, Base64.DEFAULT)
+        val bitmap = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+        holder.binding.imgUser.setImageBitmap(bitmap)
+        holder.binding.txtUserName.text = curr.userName
+
 //        holder.binding.txtRecentMessage.text = list[position].recentMessage
 //        holder.binding.txtTime.text = list[position].recentTime
 //        holder.binding.txtMessageCount.text = list[position].messageCount.toString()
